316f62e82c0f814fc809c8a46c7e1f4525b55a16
Jonathan Duarte <jonathan.duarte@medwing.com>
2020-10-28 11:27:16 +0100

    Fix test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Somethign New!
+          Learn React!
         </a>
       </header>
     </div>
